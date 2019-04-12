e5cf92f6a899b77da97fa9257884740939a665dc
Richard Jiang Yang <richard.j.yang.19@dartmouth.edu>
2019-04-12 00:15:51 -0400

    surge deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('You\'ve been on this page for 1 second.');
+
+let num = 1;
+setInterval(() => {
+  num += 1;
+  const output = `You've been on this page for ${num} seconds.`;
+  $('#main').html(output);
+}, 1000);
+
+
+/*
+var hello = 'hi';
+var output = `I wanna output ${hello}`;
+$('#main').html(output);
+*/
