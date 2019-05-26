eb4bba1bc7fd6064b93aee35be54bf76004a0739
Richard Jiang Yang <richard.j.yang.19@dartmouth.edu>
2019-05-25 22:16:01 -0400

    fixed auth bugs and added auth restrictions

---
+++ src/containers/unRequireAuth.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../style.scss';
+import { connect } from 'react-redux';
+
+export default function (ComposedComponent) {
+  class RequireAuth extends React.Component {
+    componentWillMount() {
+      if (this.props.authenticated) {
+        this.props.history.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (nextProps.authenticated) {
+        this.props.history.push('/');
+      }
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+
+  const mapStateToProps = state => (
+    {
+      authenticated: state.auth.authenticated,
+    }
+  );
+
+  return connect(mapStateToProps, null)(RequireAuth);
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,12 +6,14 @@ import { combineReducers } from 'redux';
 import ReviewReducer from './reviews-reducer';
 import UserReducer from './user-reducer';
 import SearchReducer from './search-reducer';
+import AuthReducer from './auth-reducer';
 
 const rootReducer = combineReducers({
   // count: CountReducer,
   review: ReviewReducer,
   userProfile: UserReducer,
   search: SearchReducer,
+  auth: AuthReducer,
 });
 
 export default rootReducer;
