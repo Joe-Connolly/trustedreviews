4b90ef9cf0e6dce97dc69bf5ac9df4a9a6cafc34
Soren Thompson <soren.m.thompson.20@dartmouth.edu>
2019-05-31 12:38:51 -0400

    Removed commented-out code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,12 @@
 // this defines what your store state will look like
 import { combineReducers } from 'redux';
 
-// import CountReducer from './count-reducer';
 import ReviewReducer from './reviews-reducer';
 import UserReducer from './user-reducer';
 import SearchReducer from './search-reducer';
 import AuthReducer from './auth-reducer';
 
 const rootReducer = combineReducers({
-  // count: CountReducer,
   review: ReviewReducer,
   userProfile: UserReducer,
   search: SearchReducer,
